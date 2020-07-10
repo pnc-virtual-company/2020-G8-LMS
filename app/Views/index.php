bb1c41adfbccefc38c419e9859eff25dabbdd751
sophoanteng <sophoanflowers@gmail.com>
2020-07-10 09:48:05 +0700

    #8 view html and css

---
+++ app/Views/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>LMS App</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="shortcut icon" href="images/lms_app.png" type="image/x-icon" width="500" height="600">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+      
+    <h1 class="text-success">Welcome to LMS app</h1>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
