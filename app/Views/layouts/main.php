9b920c722aaf66c3c752919f2e17523474ba8cfc
sophoanteng <sophoanflowers@gmail.com>
2020-07-10 15:25:40 +0700

    #8 layouts

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -3,15 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>LMS App</title>
+    <title>LMS App</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/custom.css">
     <link rel="shortcut icon" href="images/lms_app.png" type="image/x-icon" width="500" height="600">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
-      
-    <h1 class="text-success">Welcome to LMS app</h1>
+    <?= $this->renderSection('content') ?>
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
---
+++ app/Views/layouts/menu.php
@@ -0,0 +1,26 @@
+
+<nav class="navbar navbar-expand-md bg-img navbar-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+<!-- link menu -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="nav navbar-nav ml-auto">
+         <a class="nav-link mt-2" href="#">Your leaves</a>
+         <a class="nav-link mt-2" href="#">Leaves</a>
+         <a class="nav-link mt-2" href="#">Employees</a>
+         <a class="nav-link mt-2" href="#">Positions</a>
+         <a class="nav-link mt-2" href="#">Departments</a>
+         <a class="nav-link menu" href="#">
+            <li class="dropdown">
+         <a href="#" class="dropdown-toggle text-uppercase text-white nav-link " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" >Ronnan</a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Profile</a>
+                <a class="dropdown-item" href="#">Log out</a>
+              </div>
+          </li>
+         </a>
+    </ul>
+  </div>
+</nav>
