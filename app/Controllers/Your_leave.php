465a64e2b64adf9bcbd0da170e6a10d98fba7613
SreynetHak0506 <sreynet.hak0506@gmail.com>
2020-07-12 09:07:41 +0700

    #14 Update your leave in controller

--- app/Controllers/Your_leave.php
+++ app/Controllers/Your_leave.php
@@ -6,7 +6,21 @@ class Your_leave extends BaseController
 	{
 		return view('your_leaves/your_leaves');
 	}
-
+// add your leave
+	public function addYourLeave()
+	{
+		//code
+	}
+// edit your leave
+	public function editYourLeave()
+	{
+		//code
+	}
+// delete your leave
+	public function deleteYourLeave()
+	{
+		//code
+	}
 	//--------------------------------------------------------------------
 
 }
