6565fbf2d304dfab21fd1f6b3a982658cc17f85a
SreynetHak0506 <sreynet.hak0506@gmail.com>
2020-07-17 09:56:51 +0700

    #18  login validate

--- app/Views/users/login.php
+++ app/Views/users/login.php
@@ -13,15 +13,15 @@
       <div class="col-md-4">
         <div class="card mt-5 animated fadeIn mr-1 ml-1">
           <div class="card-body">
-            <form action="/your_leave" method="post">
+            <form action="" method="post">
               <div class="card-title text-center">
                 <img src="images/logo.png" style="width:80px" alt="Logo Telkom Indonesia" /></div>
               <div class="form-group mt-3">
                 <div class="input-group mb-3">
                   <div class="input-group-prepend">
-                    <span class="input-group-text icon-login" id="basic-addon1"><i class="material-icons">person</i></span>
+                    <span class="input-group-text icon-login" id="basic-addon1"><i class="material-icons">email</i></span>
                   </div>
-                  <input type="email" class="form-control" name="email" placeholder="Enter Mail" aria-label="Email" aria-describedby="basic-addon1" required>
+                  <input type="email" class="form-control" name="email" placeholder="Enter Mail" aria-label="Email" aria-describedby="basic-addon1" >
                 </div>
               </div>
               <div class="form-group ">
@@ -29,7 +29,7 @@
                   <div class="input-group-prepend">
                     <span class="input-group-text icon-login" id="basic-addon2"><i class="material-icons">lock</i></span>
                   </div>
-                  <input class="prepend-fx form-control" type="password" id='password' name="password" placeholder="Password" aria-label="Password" aria-describedby="basic-addon2" required>
+                  <input class="prepend-fx form-control" type="password" id='password' name="password" placeholder="Password" aria-label="Password" aria-describedby="basic-addon2">
                 </div>
               </div>
           </div>
@@ -41,8 +41,14 @@
         </div>
       </div>
       <div class="col-md-4"></div>
-
     </div>
+    <?php if(isset($message)): ?>
+            <div class="col-12 mt-5">
+              <div class="alert alert-danger" role="alert" style="width:31.5%; margin-left:34.2%">
+                <?= $message->listErrors(); ?>
+              </div>
+            </div>
+          <?php endif; ?>
   </div>
 </body>
 	<?= $this->endSection() ?>
