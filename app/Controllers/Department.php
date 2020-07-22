c09fdfa9ef11f8aa6187e0003c8a9b089ced95f0
Hy-Hong99 <hyhong179@gmail.com>
2020-07-22 16:21:54 +0700

    #44 Edit Department

--- app/Controllers/Department.php
+++ app/Controllers/Department.php
@@ -3,53 +3,59 @@ use App\Models\DepartmentModel;
 class Department extends BaseController
 {
 	//Departments List
+	protected $department;
+
+	public function __construct() 
+    {
+        $this->department = new DepartmentModel();
+    }
 	public function index()
 	{
-		$department = new DepartmentModel();
-		$allDepartments['department'] = $department->findAll();
-		return view('department/index',$allDepartments);
+		$data = [
+            'departmentData' => $this->department->getAllSubject(),
+            "copy" => "@copyright By Dy Dy"
+        ];
+		return view('department/index', $data);
 	}
 	
 	//function create departments
 	public function addDepartment()
 	{	
-		helper(['form']);
-		$data = [];
-
-		if($this->request->getMethod() == "post"){
-
-				$department = new DepartmentModel();
-				$newData = [
-					'department_name' => $this->request->getVar('department_name')
-				];
-				$department->save($newData);
-				return redirect()->to('/department');
-		}	
+		$department = $this->request->getVar('department_name');
+        $data = array(
+            'department_name' => $department
+        );
+        $this->department->insert($data);
+        return redirect()->to("/department");
 	}
 
-	//Function edit departments
-	public function editDepartment($id)
-	{	
-		$department = new DepartmentModel();
-		$data['edit']= $department->find($id);
-		return view('index',$data);
-	}
+	// //Function edit departments
+	// public function editDepartment($id)
+	// {	
+	// 	$department = new DepartmentModel();
+	// 	$data['edit']= $department->find($id);
+	// 	return view('index',$data);
+	// }
 
 	//Function update departments
    	public function updateDepartment()
 	{	
-		$department = new DepartmentModel();
-		$department->update($_POST['id'],$_POST);
-		return redirect()->to('/department');
+		$Id = $this->request->getVar('id');
+        $department = $this->request->getVar('department_name');
+        $data = array(
+            'department_name' => $department
+        );
+		$this->department->update($Id, $data);
+		var_dump($data);
+        return redirect()->to('/department');
 	}
 
 	//Function delete departments
 
 	public function deleteDepartment($id)
 	{	
-		$department = new DepartmentModel();
-		$department->delete($id);
-		return redirect()->to('/department');
+		$this->department->delete($id);
+        return redirect()->to('/department');
 	}
 
 }
