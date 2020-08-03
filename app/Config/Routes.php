933c8abf9805acaba2315e7854eebaa050e9a755
SreynetHak0506 <sreynet.hak0506@gmail.com>
2020-08-03 21:26:32 +0700

    edit authentication

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,19 +34,11 @@ $routes->setAutoRoute(true);
 $routes->add('/', 'User::index',['filter' => 'noauth']);
 $routes->add('logout', 'User::logoutUser');
 
-$routes->add('your_leave', 'Your_leave::yourLeave',['filter' => 'auth']);
 $routes->add('leave', 'Leave::showSummitedleaves',['filter' => 'auth']);
-$routes->add('employees','Employee::index',['filter' => 'auth']);
+$routes->add('positions', 'Position::position',['filter' => 'auth']);
+$routes->add('departments', 'Department::department',['filter' => 'auth']);
 
-$routes->add('position', 'Position::index',['filter' => 'auth']);
-$routes->add('department', 'Department::index',['filter' => 'auth']);
-
-$routes->add('leave', 'Leave::showSummitedleaves');//['filter' => 'noauth']);
-$routes->add('department', 'Departments::index');
-$routes->add('positions', 'Position::position');
-$routes->add('departments', 'Department::department');
-
-$routes->add('employees', 'Employee::showUser');
+$routes->add('employees', 'Employee::showUser',['filter' => 'auth']);
 $routes->add('addUser', 'Employee::createUser');
 $routes->add('remove/(:num)', 'Employee::deleteEmployee/$1');
 $routes->add('update', 'Employee::updateEmployee');
